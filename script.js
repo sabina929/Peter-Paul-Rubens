78c0266707161e8033c112b3bc0bf4bc5079f65c
Sabina <sabina.abbasova2909@gmail.com>
2021-02-04 12:46:33 +0400

    added magnetic hover to hero, about,section, alider to artworks section

--- script.js
+++ script.js
@@ -147,9 +147,94 @@ menuItemLinks.forEach(menuItemLink => {
 });
 
 
+const logo = document.querySelector(".logo");
 const navItems = document.querySelectorAll(".nav-item");
+const searchBtn = document.querySelector(".search-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const heroSection = document.querySelector(".hero-text-content");
+const heroImg = document.querySelector(".hero-img");
+const headingPrimary = document.querySelector(".heading-primary");
+const ctaBtn = document.querySelector(".cta-btn");
+const imgContainer = document.querySelector(".img-container");
+const artworks = document.querySelectorAll(".artwork");
+// const artworkInfos = document.querySelectorAll(".artwork__info");
 const tracker = document.querySelector(".cursor");
 
+// HERO SECTION
+heroSection.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+  //  console.log(this, pos)
+
+  const xWalk1 = (50 + mx*0.02)+1
+  const yWalk1 = (50 + my*0.02)+1
+  const xWalk2 = (82 + -mx*0.04)+0
+  const yWalk2 = (3 + my*0.04)+0
+  const xWalk3 = (-10.5 + mx*0.04)+0
+  const yWalk3 = (76.5 + -my*0.04)+0
+  heroImg.style.top = yWalk1 +'%';
+  heroImg.style.left = xWalk1 +'%';
+  headingPrimary.style.top = yWalk2 +'%';
+  headingPrimary.style.left = xWalk2 +'%';
+  ctaBtn.style.top = yWalk3 +'%';
+  ctaBtn.style.left = xWalk3 +'%';
+  // heroSection.style.transform = 'translate('+ mx * 0.3 +'px, '+ my * 0.6 +'px)';
+  // heroSection.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+
+})
+
+heroSection.addEventListener('mouseleave', function() {
+  heroImg.style.transform = 'translate3d(-50%, -50%, 0px)';
+  heroImg.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  headingPrimary.style.transform = 'translate3d(-50%, -50%, 0px)';
+  headingPrimary.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  ctaBtn.style.transform = 'translate3d(-50%, -50%, 0px)';
+  ctaBtn.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  // heroSection.style.transform = 'translate3d(0px, 0px, 0px)';
+  // heroSection.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+})
+
+//NAV ITEMS
+// artworks.forEach(navItem => navItem.addEventListener('mousemove', function(e) {
+//   const pos = this.getBoundingClientRect();
+//   const mx = e.clientX - pos.left - pos.width/2; 
+//   const my = e.clientY - pos.top - pos.height/2;
+//   //  console.log(this, pos)
+//   this.querySelector(".artwork__info").style.transform = 'translate('+ mx * 0.3 +'px, '+ my * 0.6 +'px)';
+//   this.querySelector(".artwork__info").style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+//   this.querySelector(".artwork__info").children[0].style.transform = 'translate('+ mx * 0.075 +'px, '+ my * 0.125 +'px)';
+
+// }));
+
+// artworks.forEach(navItem => navItem.addEventListener('mouseleave', function() {
+//   this.document.querySelector(".artwork__info").style.transform = 'translate3d(0px, 0px, 0px)';
+//   this.querySelector(".artwork__info").style.transform += 'rotate3d(0, 0, 0, 0deg)';
+//   this.querySelector(".artwork__info").children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+  
+// }));
+
+// ABOUT
+imgContainer.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+  //  console.log(this, pos)
+  this.style.transform = 'translate('+ mx * 0.3 +'px, '+ my * 0.6 +'px)';
+  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+  this.children[0].style.transform = 'translate('+ mx * 0.075 +'px, '+ my * 0.125 +'px)';
+
+  
+})
+
+imgContainer.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+
+})
+
+//NAV ITEMS
 navItems.forEach(navItem => navItem.addEventListener('mousemove', function(e) {
   const pos = this.getBoundingClientRect();
   const mx = e.clientX - pos.left - pos.width/2; 
@@ -171,6 +256,71 @@ navItems.forEach(navItem => navItem.addEventListener('mouseleave', function() {
   tracker.classList.remove('active')
 }));
 
+// SEARCH BTN
+searchBtn.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+  //  console.log(this, pos)
+  this.style.transform = 'translate('+ mx * 0.3 +'px, '+ my * 0.6 +'px)';
+  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+  this.children[0].style.transform = 'translate('+ mx * 0.075 +'px, '+ my * 0.125 +'px)';
+
+  // this.classList.add('active')
+  tracker.classList.add('active')
+})
+
+searchBtn.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+  // this.classList.remove('active')
+  tracker.classList.remove('active')
+})
+
+// MENU BTN
+menuBtn.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+  //  console.log(this, pos)
+  this.style.transform = 'translate('+ mx * 0.3 +'px, '+ my * 0.6 +'px)';
+  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+  this.children[0].style.transform = 'translate('+ mx * 0.075 +'px, '+ my * 0.125 +'px)';
+
+  // this.classList.add('active')
+  tracker.classList.add('active')
+})
+
+menuBtn.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+  // this.classList.remove('active')
+  tracker.classList.remove('active')
+})
+// LOGO
+logo.addEventListener('mousemove', function(e) {
+  const pos = this.getBoundingClientRect();
+  const mx = e.clientX - pos.left - pos.width/2; 
+  const my = e.clientY - pos.top - pos.height/2;
+  //  console.log(this, pos)
+  this.style.transform = 'translate('+ mx * 0.3 +'px, '+ my * 0.6 +'px)';
+  this.style.transform += 'rotate3d('+ mx * -0.1 +', '+ my * -0.3 +', 0, 12deg)';
+  this.children[0].style.transform = 'translate('+ mx * 0.075 +'px, '+ my * 0.125 +'px)';
+
+  // this.classList.add('active')
+  tracker.classList.add('active')
+})
+
+logo.addEventListener('mouseleave', function() {
+  this.style.transform = 'translate3d(0px, 0px, 0px)';
+  this.style.transform += 'rotate3d(0, 0, 0, 0deg)';
+  this.children[0].style.transform = 'translate3d(0px, 0px, 0px)';
+  // this.classList.remove('active')
+  tracker.classList.remove('active')
+})
+
 document.addEventListener("mousemove", e => {
   tracker.setAttribute(
     "style",
@@ -179,3 +329,39 @@ document.addEventListener("mousemove", e => {
 });
 
 
+
+
+// --------------------
+const slider = document.querySelector('.artworks-container');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.2;
+    slider.scrollLeft = scrollLeft - walk;
+});
